daae43e779565df0972f3a4350a744d9bba8f8d2
hiennguyen-corize <115438418+hiennguyen-corize@users.noreply.github.com>
2026-05-05 10:43:36 +0700

    feat: add divideNumbers utility

--- index.js
+++ index.js
@@ -19,10 +19,16 @@ function getRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+function divideNumbers(a, b) {
+  if (b === 0) throw new Error("Division by zero");
+  return a / b;
+}
+
 module.exports = {
   helloNpmMaster,
   addNumbers,
   multiplyNumbers,
   capitalizeString,
-  getRandomNumber
+  getRandomNumber,
+  divideNumbers
 };
