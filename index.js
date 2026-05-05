873a691f48183d538924cb3247a7a3a9b563d7c3
hiennguyen-corize <115438418+hiennguyen-corize@users.noreply.github.com>
2026-05-05 09:36:54 +0700

    feat: add multiplyNumbers and capitalizeString utilities

--- index.js
+++ index.js
@@ -6,7 +6,18 @@ function addNumbers(a, b) {
   return a + b;
 }
 
+function multiplyNumbers(a, b) {
+  return a * b;
+}
+
+function capitalizeString(str) {
+  if (!str) return '';
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
 module.exports = {
   helloNpmMaster,
-  addNumbers
+  addNumbers,
+  multiplyNumbers,
+  capitalizeString
 };
